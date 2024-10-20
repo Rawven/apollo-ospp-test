dc9ddc660353666194ff265f95c3df067263f4d2
liu <a2506991786@qq.com>
2024-10-20 20:03:57 +0800

    feat(): update

--- src/main/java/www/raven/ospp/TestApplication.java
+++ src/main/java/www/raven/ospp/TestApplication.java
@@ -1,8 +1,19 @@
 package www.raven.ospp;
 
+import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigService;
+
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import com.ctrip.framework.apollo.monitor.api.ApolloClientBootstrapArgsMonitorApi;
+import com.ctrip.framework.apollo.monitor.api.ApolloClientExceptionMonitorApi;
+import com.ctrip.framework.apollo.monitor.api.ApolloClientNamespaceMonitorApi;
+import com.ctrip.framework.apollo.monitor.api.ApolloClientThreadPoolMonitorApi;
+import com.ctrip.framework.apollo.monitor.api.ConfigMonitor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,7 +25,33 @@ public class TestApplication {
     SpringApplication.run(TestApplication.class, args);
     ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
     executor.scheduleAtFixedRate(() -> {
-              try {
+        try {
+            Config appConfig = ConfigService.getAppConfig();
+            log.info("AppConfig: {}", appConfig.getProperty("secret", "NoSecret"));
+            ConfigMonitor configMonitor = ConfigService.getConfigMonitor();
+            ApolloClientExceptionMonitorApi exceptionMonitorApi = configMonitor.getExceptionMonitorApi();
+            List<Exception> apolloConfigExceptionList = exceptionMonitorApi.getApolloConfigExceptionList();
+            log.info("ApolloConfigExceptionList: {}", apolloConfigExceptionList);
+            Integer exceptionCountFromStartup = exceptionMonitorApi.getExceptionCountFromStartup();
+            log.info("ExceptionCountFromStartup: {}", exceptionCountFromStartup);
+            ApolloClientNamespaceMonitorApi namespaceMonitorApi = configMonitor.getNamespaceMonitorApi();
+            Map<String, ApolloClientNamespaceMonitorApi.NamespaceMetrics> namespaceMetrics = namespaceMonitorApi.getNamespaceMetrics();
+            log.info("NamespaceMetrics: {}", namespaceMetrics);
+            ApolloClientBootstrapArgsMonitorApi runningParamsMonitorApi = configMonitor.getRunningParamsMonitorApi();
+            Map<String, Object> bootstrapArgs = runningParamsMonitorApi.getBootstrapArgs();
+            log.info("BootstrapArgs: {}", bootstrapArgs);
+            String metaLatestFreshTime = runningParamsMonitorApi.getMetaLatestFreshTime();
+            log.info("MetaLatestFreshTime: {}", metaLatestFreshTime);
+            ApolloClientThreadPoolMonitorApi threadPoolMonitorApi = configMonitor.getThreadPoolMonitorApi();
+            ApolloClientThreadPoolMonitorApi.ApolloThreadPoolInfo abstractConfigFileThreadPoolInfo = threadPoolMonitorApi.getAbstractConfigFileThreadPoolInfo();
+            log.info("AbstractConfigFileThreadPoolInfo: {}", abstractConfigFileThreadPoolInfo);
+            ApolloClientThreadPoolMonitorApi.ApolloThreadPoolInfo abstractConfigThreadPoolInfo =
+                    threadPoolMonitorApi.getAbstractConfigThreadPoolInfo();
+            log.info("ConfigServiceThreadPoolInfo: {}", abstractConfigThreadPoolInfo);
+            ApolloClientThreadPoolMonitorApi.ApolloThreadPoolInfo metricsExporterThreadPoolInfo = threadPoolMonitorApi.getMetricsExporterThreadPoolInfo();
+            log.info("MetricsExporterThreadPoolInfo: {}", metricsExporterThreadPoolInfo);
+            ApolloClientThreadPoolMonitorApi.ApolloThreadPoolInfo remoteConfigRepositoryThreadPoolInfo = threadPoolMonitorApi.getRemoteConfigRepositoryThreadPoolInfo();
+            log.info("RemoteConfigRepositoryThreadPoolInfo: {}", remoteConfigRepositoryThreadPoolInfo);
             String exporterData = ConfigService.getConfigMonitor().getExporterData();
             log.info(exporterData);
         } catch (Exception e) {
--- src/main/java/www/raven/ospp/controller/TestController.java
+++ src/main/java/www/raven/ospp/controller/TestController.java
@@ -2,13 +2,25 @@ package www.raven.ospp.controller;
 
 import com.ctrip.framework.apollo.ConfigService;
 import com.ctrip.framework.apollo.monitor.api.ConfigMonitor;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @ResponseBody
+@Slf4j
 public class TestController {
+  
+    @Value("${secret:secret}")
+    private String secret;
+    
+    @PostConstruct
+    public void init() {
+       log.info("TestController: {}", secret);  
+    }
 
   @GetMapping("/metrics")
   public String metrics() {
